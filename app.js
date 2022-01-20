d00fdc759cadf8a4d7d95b7813890cc3e1c3e674
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2022-01-21 08:40:52 +1300

    remove unnecessary space

--- app.js
+++ app.js
@@ -123,7 +123,6 @@ app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/d
 app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }));
 app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), { maxAge: 31557600000 }));
 
-
 /**
  * Primary app routes.
  */
